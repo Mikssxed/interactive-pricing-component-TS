ee51214f642d7de2d1e51ec1cdf44eb54b8fafe4
Mikssxed <112573426+Mikssxed@users.noreply.github.com>
2022-12-05 20:57:21 +0100

    Add files via upload

---
+++ dist/app.js
@@ -0,0 +1,48 @@
+const slider = document.querySelector('input[type="range"]');
+const views = document.querySelector("#pvNumber");
+const money = document.querySelector("#monthNumber");
+const switcher = document.querySelector("#switch");
+const discount = document.querySelector("#circle");
+let array = [8, 12, 16, 24, 36];
+let arrayDisc = [];
+const reduce = () => {
+    arrayDisc = [];
+    if (discount.classList.contains("toggled")) {
+        for (let i = 0; i <= 4; i++) {
+            arrayDisc.push(Math.round(Number((array[i] * 0.75).toPrecision(2))));
+        }
+    }
+    else {
+        arrayDisc = array;
+    }
+};
+const slide = () => {
+    reduce();
+    let sliderValue = Number(slider.value);
+    if (sliderValue <= 20) {
+        views.innerHTML = "10K";
+        money.innerHTML = `${arrayDisc[0]}.00`;
+    }
+    else if (sliderValue <= 40) {
+        views.innerHTML = "50K";
+        money.innerHTML = `${arrayDisc[1]}.00`;
+    }
+    else if (sliderValue <= 60) {
+        views.innerHTML = "100K";
+        money.innerHTML = `${arrayDisc[2]}.00`;
+    }
+    else if (sliderValue <= 80) {
+        views.innerHTML = "500K";
+        money.innerHTML = `${arrayDisc[3]}.00`;
+    }
+    else {
+        views.innerHTML = "1M";
+        money.innerHTML = `${arrayDisc[4]}.00`;
+    }
+    slider.style.backgroundSize = `${slider.value}%`;
+};
+switcher.addEventListener("click", (change) => {
+    discount.classList.toggle("toggled");
+    slide();
+});
+slider.addEventListener("input", slide);
